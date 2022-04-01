c46702e8d28e7903f60dc4bc9b543ef6aafa6b6f
mins1031 <urisegea@naver.com>
2022-04-01 17:57:54 +0900

    lab : 이넘 리스트 테스트

---
+++ src/main/java/enumtype/Role.java
@@ -0,0 +1,25 @@
+package enumtype;
+
+public enum Role {
+    WEB_DESIGN("웹디자인"),
+    APP_DESIGN("앱디자인"),
+    PUBLISING_EDIT_DESIGN("출판/편집 디자인"),
+    GAME_DESIGN("게임 디자인"),
+    FREEBIE_DESIGN("판촉물 디자인"),
+    THREE_DIMENSIONS_DESIGN("3D 디자인"),
+    GRAPHIC_DESIGN("그래픽 디자인"),
+    PACKAGE_DESIGN("패키지 디자인"),
+    ART_DIRECTION("아트 디렉션"),
+    ANIMATION("애니메이션"),
+    LOGO_BRANDING("로고 브랜딩");
+
+    private String desc;
+
+    Role(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
