349322752ed0d35e49b51063d26c0b876e95aa95
mins1031 <urisegea@naver.com>
2022-07-15 23:30:12 +0900

    map 실험

--- src/main/java/maplab/MapLab1.java
+++ src/main/java/maplab/MapLab1.java
@@ -1,10 +1,39 @@
 package maplab;
 
+import programmers.MatrixPlus;
+
+import javax.sql.rowset.serial.SerialStruct;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapLab1 {
+
+    public void countDupliment() {
+        String s = "aaaBcCdcd";
+        String[] split = s.split("");
+
+        Set<String> hashSet = new HashSet<>(Arrays.asList(split));
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String s1 : split) {
+            if (hashSet.contains(s1)) {
+                if (!map.containsKey(s1)) {
+                    map.put(s1, 1);
+                    continue;
+                }
+
+                Integer integer = map.get(s1);
+                map.put(s1, integer + 1);
+            }
+        }
+
+        System.out.println(map);
+
+    }
+
 
     public static void main(String[] args) {
         Map<String, Integer> map = new HashMap<>();
@@ -20,5 +49,7 @@ public class MapLab1 {
         for (String string : strings) {
             System.out.println(string);
         }
+
+        new MapLab1().countDupliment();
     }
 }
