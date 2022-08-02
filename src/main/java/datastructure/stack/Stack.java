083c3080fc56fcd53908f7270d28dc19e774252c
mins1031 <urisegea@naver.com>
2022-08-02 23:11:55 +0900

    스택 구현 중

--- src/main/java/datastructure/stack/Stack.java
+++ src/main/java/datastructure/stack/Stack.java
@@ -1,12 +1,23 @@
 package datastructure.stack;
 
-public class Stack<T> {
+public class Stack {
 
     private Object [] array;
     private int top = 0;
+    private int size = 0;
 
-    public Stack(int top) {
-        this.array = new Object[top];
+    public Stack(int size) {
+        this.top = -1;
+        this.array = new Object[size];
+        this.size = size;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public boolean isFull() {
+        return this.top == this.size - 1;
     }
 
 
