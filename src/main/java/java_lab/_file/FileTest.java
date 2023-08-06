c4325edaf2d82057b654bad1b2c83b473228e0f0
mins1031 <urisegea@naver.com>
2023-08-06 22:14:11 +0900

    try - with - resource test

--- src/main/java/java_lab/_file/FileTest.java
+++ src/main/java/java_lab/_file/FileTest.java
@@ -1,6 +1,8 @@
 package java_lab._file;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -27,6 +29,30 @@ public class FileTest {
                 retList.add(file.getName());    // 이름출력
                 System.out.println(file.getName());
             }
+        }
+    }
+
+    public void writeFile() {
+        FileWriter file = null;
+        try {
+            file = new FileWriter("data.txt");
+            file.write("Hello World");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                file.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void writeFile2() {
+        try (FileWriter file = new FileWriter("data.txt")) {
+            file.write("Hello World");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
