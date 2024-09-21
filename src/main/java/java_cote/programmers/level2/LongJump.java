18b4c4b4f52c14ce0b9e3f888e4e37e0b67bdacb
mins1031 <urisegea@naver.com>
2024-09-21 19:50:54 +0900

    멀리뛰기 - level2

--- src/main/java/java_cote/programmers/level2/LongJump.java
+++ src/main/java/java_cote/programmers/level2/LongJump.java
@@ -2,17 +2,28 @@ package java_cote.programmers.level2;
 
 public class LongJump {
     public long solution(int n) {
-        long answer = 0;
-        //1칸은 숫자 +
+        // DP 문제
+        int[] dp = new int[n+1];
+        if (n == 1 || n ==2) {
+            return n;
+        }
 
+        dp[1] = 1;
+        dp[2] = 2;
 
-        return answer;
+        int loopStart = 3;
+
+        for (int i = loopStart; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        }
+
+        return dp[n];
     }
 
     public static void main(String[] args) {
         LongJump longJump = new LongJump();
         int n1 = 5;
-        int n2 = 3;
+        int n2 = 6;
 
         long solution1 = longJump.solution(n1);
         long solution2 = longJump.solution(n2);
