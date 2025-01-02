b249d005f699359dbebd45819bd2f4b3b260f1a7
mins1031 <urisegea@naver.com>
2025-01-02 23:50:36 +0900

    level2 - 마법의 엘리베이터

---
+++ src/main/java/java_cote/programmers/level2/MagicElevator.java
@@ -0,0 +1,16 @@
+package java_cote.programmers.level2;
+
+public class MagicElevator {
+    public int solution(int storey) {
+        int answer = 0;
+        
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int storey = 16;
+        MagicElevator magicElevator = new MagicElevator();
+        int solution = magicElevator.solution(storey);
+        System.out.println(solution);
+    }
+}
