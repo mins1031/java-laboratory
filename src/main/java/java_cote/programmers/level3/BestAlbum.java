c702c969f95c6dc85ccaa1dbde30becca3b2c6b8
mins1031 <urisegea@naver.com>
2025-01-13 22:47:18 +0900

    level3 - 베스트엘범(BFS/DFS)

---
+++ src/main/java/java_cote/programmers/level3/BestAlbum.java
@@ -0,0 +1,25 @@
+package java_cote.programmers.level3;
+
+public class BestAlbum {
+    private static class AlbumGenre {
+        private int totalPlay;
+        private List<>
+    }
+
+    public int[] solution(String[] genres, int[] plays) {
+        int[] answer = {};
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
+        int[] plays = {500, 600, 150, 800, 2500};
+        BestAlbum bestAlbum = new BestAlbum();
+        int[] solution = bestAlbum.solution(genres, plays);
+        for (int i : solution) {
+            System.out.println(i);
+        }
+    }
+}
--- src/main/java/java_cote/programmers/level3/TravelPath.java
+++ src/main/java/java_cote/programmers/level3/TravelPath.java
@@ -4,10 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TravelPath {
+    private static class Path {
+        public String department;
+        public String destination;
+
+        public Path(String department, String destination) {
+            this.department = department;
+            this.destination = destination;
+        }
+    }
+
     public String[] solution(String[][] tickets) {
         String[] answer = {};
+        int depth = tickets.length + 1;
         List<String> result = new ArrayList<>();
-
 
         return answer;
     }
