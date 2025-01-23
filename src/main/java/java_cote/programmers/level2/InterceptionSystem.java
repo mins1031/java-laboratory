fdaf790985216a441a89a37953d18808fe2b495d
mins1031 <urisegea@naver.com>
2025-01-23 23:53:06 +0900

    level2 - 요격 시스템..

---
+++ src/main/java/java_cote/programmers/level2/InterceptionSystem.java
@@ -0,0 +1,19 @@
+package java_cote.programmers.level2;
+
+public class InterceptionSystem {
+    public int solution(int[][] targets) {
+        int answer = 0;
+        int[] points = new int[100000000];
+        
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[][] targets = {{4,5},{4,8},{10,14},{11,13},{5,12},{3,7},{1,4}};
+
+        InterceptionSystem interceptionSystem = new InterceptionSystem();
+        int solution = interceptionSystem.solution(targets);
+        System.out.println(solution);
+    }
+}
