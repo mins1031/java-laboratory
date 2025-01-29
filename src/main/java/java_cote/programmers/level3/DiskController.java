f8fd6511e233b83c6de84ab5d54b2b97f3ea3d99
mins1031 <urisegea@naver.com>
2025-01-30 00:49:14 +0900

    level3 - 디스크 컨트롤러(우선순위큐 - 힙) + 앞으로 애매하다 싶으면 무조건 주석으로 생각 정리하면서 풀어보는데 상태기반으로 구조화 사고 도입해서 고민해보자

--- src/main/java/java_cote/programmers/level3/DiskController.java
+++ src/main/java/java_cote/programmers/level3/DiskController.java
@@ -1,7 +1,11 @@
 package java_cote.programmers.level3;
 
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.PriorityQueue;
 
@@ -31,74 +35,63 @@ public class DiskController {
         }
     }
 
-
-    //1. 소요시간이 짧은것
-    //2. 요청시각이 빠른것
-    //3. 번호가 작은것
+    //job map int - list로 손보고 jobcount 도 필요
+    //현시점 처리전 현시점과 동일한 시점에 시작되야할 job을 대기큐에 넣는 작업필요
+    //이후 현시점 상태처리 프로세스
+    //1. 동작하는 job이 없는경우
+        //1-1. 대기큐에 job이 있으면?
+            //1-1-1. 있으면 꺼내서 현 job에 설정
+            //1-1-2. 전체시간, 수행시간, jobcount++
+            //1-1-3. continue
+        //1-2. 대기큐에 job이 없으면?
+            //1-2-1. 전체시간 ++ 후 continue
+    //2. 동작하는 job이 있는경우
+        //2-1. 수행시간이 == job 소요시간  과 동일한경우
+            //2-1-1. 현 job null 처리, 수행시간 초기화
+            //2-1-2. answer에 현시간 - 요청시간 추가
+            //2-1-3. continue
+        //2-2. 수행시간이 < job 소요시간  보다 적은경우
+            //2-2-1. 수행시간++, 전체시간++
+            //2-2-2. continue
+        //2-3. 만약 위 처리후 jobCount == jobs.length 인경우 break
     public int solution(int[][] jobs) {
         int answer = 0;
         PriorityQueue<DiskJob> waitQueue = new PriorityQueue<>(setJobSortConditions());
-        //job map int - list로 손보고 jobcount 도 필요
-        //현시점 처리전 현시점과 동일한 시점에 시작되야할 job을 대기큐에 넣는 작업필요
-        //이후 현시점 상태처리 프로세스
-            //1. 동작하는 job이 없는경우
-                //1-1. 대기큐에 job이 있으면?
-                    //1-1-1. 있으면 꺼내서 현 job에 설정
-                    //1-1-2. 현 시간값, 수행시간, jobcount++
-                    //1-1-3. continue
-                //1-2. 대기큐에 job이 없으면?
-                    //1-2-1 현 시간값 ++ 후 continue
-            //2. 동작하는 job이 있는경우
-                //2-1.
-
-
+        Map<Integer, List<DiskJob>> jobsMap = initJobsMap(jobs);
 
         DiskJob presentJob = null;
+        boolean isMiddleTime = false;
         int totalExecutionCount = 0;
         int executionTimePerJob = 0;
         int jobCount = 0;
-
-        // 현재 동작중인 job이 있고 해당시점에 인입되는 job이 있으면 대기큐에 insert
-        // 현재 동작중인 job이 없고 해당시점에 인입되는 job이 있으면 대기큐로 무조건 insert 한다음 카운트 안늘리고 바로 대기큐에서 뽑으면 됨.
         while (true) {
-            if (Objects.isNull(presentJob)) {
-                // 현재 동작중인 job은 없지만
-                if (jobs[jobCount][0] == totalExecutionCount) {
-                    waitQueue.add(
-                            new DiskJob(
-                                    jobCount,
-                                    jobs[jobCount][0],
-                                    jobs[jobCount][1]
-                            )
-                    );
+            addThisTimeJobToQueue(waitQueue, jobsMap, totalExecutionCount, isMiddleTime);
 
+            if (Objects.isNull(presentJob)) {
+                if (waitQueue.isEmpty()) {
+                    totalExecutionCount++;
+                    isMiddleTime = false;
                     continue;
                 }
 
-//                presentJob = new DiskJob(
-//                        jobCount,
-//                        jobs[jobCount][0],
-//                        jobs[jobCount][1]
-//                );
-//                jobCount++;
-//                totalExecutionCount++;
-//                executionTimePerJob++;
+                presentJob = waitQueue.poll();
+                totalExecutionCount++;
+                executionTimePerJob++;
+                jobCount++;
+                isMiddleTime = false;
                 continue;
             }
 
-            if (Objects.nonNull(presentJob)) {
-                if (jobs[jobCount][0] == totalExecutionCount) {
-                    waitQueue.add(
-                            new DiskJob(
-                                    jobCount,
-                                    jobs[jobCount][0],
-                                    jobs[jobCount][1]
-                            )
-                    );
-
-                    continue;
-                }
+            if (presentJob.processTime > executionTimePerJob) {
+                executionTimePerJob++;
+                totalExecutionCount++;
+                continue;
             }
+
+            answer += presentJob.calculateTotalJobTime(totalExecutionCount);
+            executionTimePerJob = 0;
+            presentJob = null;
+            isMiddleTime = true;
 
             if (jobCount == jobs.length) {
                 break;
@@ -109,6 +102,40 @@ public class DiskController {
         return answer / jobs.length;
     }
 
+    //1. 소요시간이 짧은것
+    //2. 요청시각이 빠른것
+    //3. 번호가 작은것
+    private void addThisTimeJobToQueue(PriorityQueue<DiskJob> waitQueue, Map<Integer, List<DiskJob>> jobsMap, int totalExecutionCount, boolean isMiddleTime) {
+        if (isMiddleTime) {
+            return;
+        }
+
+        List<DiskJob> diskJobs = jobsMap.get(totalExecutionCount);
+        if (Objects.nonNull(diskJobs)) {
+            waitQueue.addAll(diskJobs);
+        }
+    }
+
+    private Map<Integer, List<DiskJob>> initJobsMap(int[][] jobs) {
+        Map<Integer, List<DiskJob>> jobsMap = new HashMap<>();
+        int seq = 0;
+        for (int[] job : jobs) {
+            int reqTime = job[0];
+            int processTime = job[1];
+            List<DiskJob> diskJobs = jobsMap.getOrDefault(reqTime, new ArrayList<>());
+            diskJobs.add(
+                    new DiskJob(
+                            seq,
+                            reqTime,
+                            processTime
+                    )
+            );
+            jobsMap.put(reqTime, diskJobs);
+            seq++;
+        }
+
+        return jobsMap;
+    }
     private Comparator<DiskJob> setJobSortConditions() {
         return (o1, o2) -> {
             if (o1.processTime > o2.processTime) {
@@ -130,41 +157,11 @@ public class DiskController {
         };
     }
 
-    private void initQueue(int[][] jobs, PriorityQueue<DiskJob> waitQueue) {
-        waitQueue.add(
-                new DiskJob(
-                        0,
-                        jobs[0][0],
-                        jobs[0][1]
-                )
-        );
-
-//        for (int i = 0; i < jobs.length; i++) {
-//            DiskJob diskJob = new DiskJob(
-//                    i,
-//                    jobs[i][0],
-//                    jobs[i][1]
-//            );
-//
-//            waitQueue.add(diskJob);
-//        }
-    }
-
-    private int compareProcessTime(DiskJob o1, DiskJob o2) {
-        if (o1.processTime > o2.processTime) {
-            return 1;
-        } else if (o1.processTime < o2.processTime) {
-            return -1;
-        }
-
-        return 0;
-    }
-
     public static void main(String[] args) {
-        int[][] jobs = {{0, 3}, {1, 9}, {3, 5}};
+//        int[][] jobs = {{0, 3}, {1, 9}, {3, 5}};
 //        int[][] jobs = {{5, 10}, {6, 8}, {14, 2}, {11, 5}, {100, 7}}; // 11
 //        int[][] jobs = {{0, 1}, {2, 2}, {2, 3}}; // 2
-//        int[][] jobs = {{0, 3}, {4, 4}, {5, 3}, {7, 1}}; // 4
+        int[][] jobs = {{0, 3}, {4, 4}, {5, 3}, {7, 1}}; // 4
 
 
         DiskController diskController = new DiskController();
