849324228b7e053925ef218014e635cc4e509349
mins1031 <urisegea@naver.com>
2025-01-29 23:53:33 +0900

    level3 - 디스크 컨트롤러(우선순위큐 - 힙)

--- src/main/java/java_cote/programmers/level3/DiskController.java
+++ src/main/java/java_cote/programmers/level3/DiskController.java
@@ -1,6 +1,7 @@
 package java_cote.programmers.level3;
 
 
+import java.util.Comparator;
 import java.util.Objects;
 import java.util.PriorityQueue;
 
@@ -36,7 +37,80 @@ public class DiskController {
     //3. 번호가 작은것
     public int solution(int[][] jobs) {
         int answer = 0;
-        PriorityQueue<DiskJob> waitQueue = new PriorityQueue<>((o1, o2) -> {
+        PriorityQueue<DiskJob> waitQueue = new PriorityQueue<>(setJobSortConditions());
+        //job map int - list로 손보고 jobcount 도 필요
+        //현시점 처리전 현시점과 동일한 시점에 시작되야할 job을 대기큐에 넣는 작업필요
+        //이후 현시점 상태처리 프로세스
+            //1. 동작하는 job이 없는경우
+                //1-1. 대기큐에 job이 있으면?
+                    //1-1-1. 있으면 꺼내서 현 job에 설정
+                    //1-1-2. 현 시간값, 수행시간, jobcount++
+                    //1-1-3. continue
+                //1-2. 대기큐에 job이 없으면?
+                    //1-2-1 현 시간값 ++ 후 continue
+            //2. 동작하는 job이 있는경우
+                //2-1.
+
+
+
+        DiskJob presentJob = null;
+        int totalExecutionCount = 0;
+        int executionTimePerJob = 0;
+        int jobCount = 0;
+
+        // 현재 동작중인 job이 있고 해당시점에 인입되는 job이 있으면 대기큐에 insert
+        // 현재 동작중인 job이 없고 해당시점에 인입되는 job이 있으면 대기큐로 무조건 insert 한다음 카운트 안늘리고 바로 대기큐에서 뽑으면 됨.
+        while (true) {
+            if (Objects.isNull(presentJob)) {
+                // 현재 동작중인 job은 없지만
+                if (jobs[jobCount][0] == totalExecutionCount) {
+                    waitQueue.add(
+                            new DiskJob(
+                                    jobCount,
+                                    jobs[jobCount][0],
+                                    jobs[jobCount][1]
+                            )
+                    );
+
+                    continue;
+                }
+
+//                presentJob = new DiskJob(
+//                        jobCount,
+//                        jobs[jobCount][0],
+//                        jobs[jobCount][1]
+//                );
+//                jobCount++;
+//                totalExecutionCount++;
+//                executionTimePerJob++;
+                continue;
+            }
+
+            if (Objects.nonNull(presentJob)) {
+                if (jobs[jobCount][0] == totalExecutionCount) {
+                    waitQueue.add(
+                            new DiskJob(
+                                    jobCount,
+                                    jobs[jobCount][0],
+                                    jobs[jobCount][1]
+                            )
+                    );
+
+                    continue;
+                }
+            }
+
+            if (jobCount == jobs.length) {
+                break;
+            }
+        }
+
+
+        return answer / jobs.length;
+    }
+
+    private Comparator<DiskJob> setJobSortConditions() {
+        return (o1, o2) -> {
             if (o1.processTime > o2.processTime) {
                 return 1;
             } else if (o1.processTime < o2.processTime) {
@@ -53,45 +127,27 @@ public class DiskController {
                     return -1;
                 }
             }
-        });
+        };
+    }
 
-        for (int i = 0; i < jobs.length; i++) {
-            DiskJob diskJob = new DiskJob(
-                    i,
-                    jobs[i][0],
-                    jobs[i][1]
-            );
+    private void initQueue(int[][] jobs, PriorityQueue<DiskJob> waitQueue) {
+        waitQueue.add(
+                new DiskJob(
+                        0,
+                        jobs[0][0],
+                        jobs[0][1]
+                )
+        );
 
-            waitQueue.add(diskJob);
-        }
-
-        DiskJob presentJob = null;
-        int totalExecutionCount = 0;
-        int executionTimePerJob = 0;
-        // TODO 이거구현 다시 해야한다. 미리 큐에 다 넣어놓고 구현하는게 아니라 요청시간 별로도 체크해서 시간 구해야함. -> 아래 반례 보면 금방 판단 가능
-        while (Objects.nonNull(presentJob) || !waitQueue.isEmpty()) {
-            if (Objects.isNull(presentJob)) {
-                presentJob = waitQueue.poll();
-                continue;
-            }
-
-            if (presentJob.reqTime > totalExecutionCount) {
-                totalExecutionCount = presentJob.reqTime;
-                continue;
-            }
-
-            if (presentJob.processTime > executionTimePerJob) {
-                executionTimePerJob++;
-                totalExecutionCount++;
-                continue;
-            }
-
-            answer += presentJob.calculateTotalJobTime(totalExecutionCount);
-            executionTimePerJob = 0;
-            presentJob = null;
-        }
-
-        return answer / jobs.length;
+//        for (int i = 0; i < jobs.length; i++) {
+//            DiskJob diskJob = new DiskJob(
+//                    i,
+//                    jobs[i][0],
+//                    jobs[i][1]
+//            );
+//
+//            waitQueue.add(diskJob);
+//        }
     }
 
     private int compareProcessTime(DiskJob o1, DiskJob o2) {
@@ -105,8 +161,8 @@ public class DiskController {
     }
 
     public static void main(String[] args) {
-//        int[][] jobs = {{0, 3}, {1, 9}, {3, 5}};
-        int[][] jobs = {{5, 10}, {6, 8}, {14, 2}, {11, 5}, {100, 7}}; // 11
+        int[][] jobs = {{0, 3}, {1, 9}, {3, 5}};
+//        int[][] jobs = {{5, 10}, {6, 8}, {14, 2}, {11, 5}, {100, 7}}; // 11
 //        int[][] jobs = {{0, 1}, {2, 2}, {2, 3}}; // 2
 //        int[][] jobs = {{0, 3}, {4, 4}, {5, 3}, {7, 1}}; // 4
 
