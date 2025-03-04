82d3bd047f4a306bb22802f94b7afb23c69c6db5
mins1031 <urisegea@naver.com>
2025-03-05 00:00:47 +0900

    level3 - 부대복귀 (BFS)

--- src/main/java/java_cote/programmers/level3/ReturnArmyPoint.java
+++ src/main/java/java_cote/programmers/level3/ReturnArmyPoint.java
@@ -1,83 +1,124 @@
 package java_cote.programmers.level3;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Queue;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ReturnArmyPoint {
-    public static class Node{
-        private final int department;
-        private final int arrivement;
+    public static class QueueWrapper{
+        private Queue<Integer> queue;
+        private Set<Integer> queueSet;
 
-        public Node(int department, int arrivement) {
-            this.department = department;
-            this.arrivement = arrivement;
+        public QueueWrapper() {
+            this.queue = new LinkedList<>();
+            this.queueSet = new HashSet<>();
+        }
+
+        public void add(Integer value) {
+            if (queueSet.contains(value)) {
+                return;
+            }
+
+            queue.add(value);
+            queueSet.add(value);
+        }
+
+        public Integer poll() {
+            Integer poll = queue.poll();
+            queueSet.remove(poll);
+
+            return poll;
+        }
+
+        public boolean isEmpty() {
+            return this.queue.isEmpty();
+        }
+
+        public void clear() {
+            this.queueSet.clear();
+            this.queue.clear();
         }
     }
 
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
         List<Integer> answerList = new ArrayList<>();
-        Map<Integer, List<Node>> map = new HashMap<>();
+        int[] sourceValues = new int[501];
+        Map<Integer, Set<Integer>> map = new HashMap<>();
         for (int[] road : roads) {
-            List<Node> value = map.getOrDefault(road[0], new ArrayList<>());
-            value.add(new Node(road[0], road[1]));
+            Set<Integer> firstValue = map.getOrDefault(road[0], new HashSet<>());
+            firstValue.add(road[1]);
+            map.put(road[0], firstValue);
 
-            map.put(road[0], value);
+            Set<Integer> secondValue = map.getOrDefault(road[1], new HashSet<>());
+            secondValue.add(road[0]);
+            map.put(road[1], secondValue);
+        }
+
+        for (int i = 0; i < sourceValues.length; i++) {
+            sourceValues[i] = -2;
         }
 
         for (int source : sources) {
-            if (source == destination) {
-                answerList.add(0);
+            if (destination == source) {
+                sourceValues[source] = 0;
                 continue;
             }
             if (Objects.isNull(map.get(source))) {
-                answerList.add(-1);
+                sourceValues[source] = -1;
+            }
+        }
+
+        Set<Integer> notDupleSources = Arrays.stream(sources).boxed().collect(Collectors.toSet());
+        QueueWrapper queueWrapper = new QueueWrapper();
+        queueWrapper.add(destination);
+
+        // 맵에서 출발지 정보로 조회해서 리스트 돌려보고 목적지 있으면 뎁스 적용해서 큐비우고 탈출하고 없으면 전체목록 큐에 추가하고 뎁스 추가해서 다음 뎁스 확인해야지
+        // 소스에서 뽑은 숫자기반으로 맵에서 리스트 추출,
+        // 추출된 리스트내에 목적지 있으면 최단 경로에 depth추가하고 큐 초기화하고 탈출
+        // 없으면 큐에 모든 도착지 넣어놓고 다음 큐 이동
+        // 다음 큐에선 또 그 도착지 정보가 출발지인 애들 맵에서 뽑아서 도착지 있는지 확인
+        // 만나는 애들마다 몇번째 뎁스인지 인덱스찾아가서 설정
+
+        // 위 전략은 실패. 애초에 양방향 그래프로 앞에 있는 값을 키로 map을 만드는 것부터가 틀린 전제임.
+        int depth = 1;
+        while (!queueWrapper.isEmpty()) {
+            if (depth == n) {
+                queueWrapper.clear();
+                break;
+            }
+
+            Integer poll = queueWrapper.poll();
+            Set<Integer> nodes = map.get(poll);
+            for (Integer node : nodes) {
+                if (notDupleSources.contains(node)) {
+                    if (sourceValues[node] != -2) {
+                        continue;
+                    }
+
+                    sourceValues[node] = depth;
+                }
+
+                queueWrapper.add(node);
+            }
+
+            depth++;
+        }
+
+        for (int idx : sources) {
+            if (idx == destination) {
+                answerList.add(0);
                 continue;
             }
 
-            Queue<Integer> queue = new LinkedList<>();
-            queue.add(map.get(source).get(0).department);
-
-            // 맵에서 출발지 정보로 조회해서 리스트 돌려보고 목적지 있으면 뎁스 적용해서 큐비우고 탈출하고 없으면 전체목록 큐에 추가하고 뎁스 추가해서 다음 뎁스 확인해야지
-            // 소스에서 뽑은 숫자기반으로 맵에서 리스트 추출,
-            // 추출된 리스트내에 목적지 있으면 최단 경로에 depth추가하고 큐 초기화하고 탈출
-            // 없으면 큐에 모든 도착지 넣어놓고 다음 큐 이동
-            // 다음 큐에선 또 그 도착지 정보가 출발지인 애들 맵에서 뽑아서 도착지 있는지 확인
-            int depth = 1;
-            int shortestPath = 0;
-            boolean isExit = false;
-            while (!queue.isEmpty()) {
-                if (isExit) {
-                    break;
-                }
-
-                if (depth == n) {
-                    shortestPath = depth;
-                    queue.clear();
-                    break;
-                }
-
-                Integer poll = queue.poll();
-                List<Node> nodes = map.get(poll);
-                for (Node node : nodes) {
-                    if (node.arrivement == destination || node.department == destination) {
-                        shortestPath = depth;
-                        queue.clear();
-                        isExit = true;
-                        break;
-                    }
-
-                    queue.add(node.arrivement);
-                }
-
-                depth++;
-            }
-
-            answerList.add(shortestPath);
+            answerList.add(sourceValues[idx]);
         }
 
         return answerList.stream().mapToInt(Integer::intValue).toArray();
