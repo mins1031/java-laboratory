e1f2f620c2d357ca992999e62906489911dc8744
mins1031 <urisegea@naver.com>
2025-04-21 00:53:29 +0900

    level3 - 다단계 칫솔 판매

---
+++ src/main/java/java_cote/programmers/level3/ToothBrushSeller.java
@@ -0,0 +1,15 @@
+package java_cote.programmers.level3;
+
+public class ToothBrushSeller {
+    // 배열에 바로 상위 계층 부모 정보를 저장하는 방식으로 처리할까함. 상위가 없으면 자기자신의 인덱스 값을 갖고 있고 부모가 있다면 상위 인덱스의 값을 가지고 있는 형태.
+        // 배열이 구현도 쉬울것 같고 상위 계층 재귀로 찾아가면서 관련 로직 처리도 명료할것 같음.
+
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
